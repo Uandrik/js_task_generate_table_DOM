63a8f45062196e0cfab0c1b441f8defac940ae0f
Uandrik <uandriy4141@gmail.com>
2025-03-05 20:17:47 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -356,9 +356,9 @@ const people = [
 
 // eslint-disable-next-line no-console
 // you can remove it
+const table = document.querySelector('.dashboard');
 
 people.forEach((person) => {
-  const table = document.querySelector('.dashboard');
   const tableRow = document.createElement('tr');
   let gender = '';
   const age = person.died - person.born;
